7ca7a0951530e175f402f90874d9a7b6008dd032
Jessica Wilson <jwilson@zipwhip.com>
2019-12-17 19:09:39 -0800

    Added js exec to execute git command

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { exec } = require('child_process');
 
 try {
   // `regex-string` input defined in action metadata file
   const regexString = core.getInput('regex-string');
-  console.log(`Regex String: ${regexString}!`);
-
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  console.log(`Regex String: ${regexString}`);
   
+  // Get all the branches with the regex prefix and return the last version
+  exec('git ls-remote --heads origin  | sed `s?.*refs/heads/${regexString}`', (err, branches, stderr) => {
+    if (err) {
+        console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
+        console.log('\x1b[31m%s\x1b[0m', stderr);
+        process.exit(1);
+
+        return;
+    }
+
+    console.log('\x1b[32m%s\x1b[0m', `Found branches: ${branches}`);
+
+    // core.setOutput("release-branch-name", release-branch-name);
+    console.log(`The branches list: ${branches}`);
+    // console.log(`::set-output name=tag::${tag}`);
+    process.exit(0);
+});
+  
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
