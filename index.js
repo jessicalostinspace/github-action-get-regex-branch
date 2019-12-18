8d22910bc700721835ef368e5c9c43c21146bcd9
Jessica Wilson <jwilson@zipwhip.com>
2019-12-17 21:58:47 -0800

    Console logged everything

--- index.js
+++ index.js
@@ -6,28 +6,34 @@ try {
   // `regex-string` input defined in action metadata file
   const regexString = core.getInput('regex-string');
   console.log(`Regex String: ${regexString}`);
+
+  const command = 'git for-each-ref --sort=-committerdate refs/heads/release-v';
   
   // Get all the branches with the regex prefix and return the last version
-  const releaseBranch = exec('git for-each-ref --sort=-committerdate refs/heads/release-v', (err, branches, stderr) => {
-      console.log('err', err)
-      console.log('branches', branches)
-    if (err) {
-        console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
-        console.log('\x1b[31m%s\x1b[0m', stderr);
-        process.exit(1);
+  const lastSemVer = (command, cb) => {
+      const lastReleaseBranchName = exec(command, (err, stdout, stderr) => {
+        console.log('err', err)
+        console.log('branches', stdout)
+        if (err) {
+            console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
+            console.log('\x1b[31m%s\x1b[0m', stderr);
+            process.exit(1);
 
-        return;
-    }
+            return;
+        }
 
-    console.log('\x1b[32m%s\x1b[0m', `Found branches: ${branches}`);
+        console.log('\x1b[32m%s\x1b[0m', `Found branches: ${stdout}`);
 
-    // core.setOutput("release-branch-name", release-branch-name);
-    console.log(`The branches list: ${branches}`);
-    // console.log(`::set-output name=tag::${tag}`);
-    process.exit(0);
-});
+        // core.setOutput("release-branch-name", release-branch-name);
+        console.log(`The branches list: ${branches}`);
+        // console.log(`::set-output name=tag::${tag}`);
+        process.exit(0);
+    })
 
-console.log('release branch', releaseBranch)
+    console.log('lastReleaseBranchName: ', lastReleasBranchName)
+  };
+
+console.log('lastSemVer', lastSemVer);
   
 //   const time = (new Date()).toTimeString();
 //   core.setOutput("time", time);
