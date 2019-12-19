52df654f9b1a2c811fe874682ef0c902eeb06fef
Jessica Wilson <jwilson@zipwhip.com>
2019-12-19 13:39:05 -0800

    debugging

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ try {
   console.log(`Regex String: ${regexString}`);
 
 //   const command = '"sh ./cut-release-branch.sh" ${regexString}';
-  const command = 'echo pwd'
+  const command = 'echo $pwd'
   
   // Get all the branches with the regex prefix and return the last version
     exec(command, (err, stdout, stderr) => {
