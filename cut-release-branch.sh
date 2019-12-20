ab3520368289e3f16b7d79688a786f215dfcd0e5
Jessica Wilson <jwilson@zipwhip.com>
2019-12-20 11:03:57 -0800

    clean up

--- cut-release-branch.sh
+++ cut-release-branch.sh
@@ -3,10 +3,14 @@ set -e
 
 regexString=$1
 
-echo 'git for each: '$(git for-each-ref --sort=committerdate refs/heads/)
-
+semanticVersion=''
 if [ $regexString ]; then 
-    echo $(git branch -a | grep $regexString | egrep -o '([0-9]+\.){2}[0-9]+')
+    semanticVersion=$(echo $(git branch -a | grep $regexString | egrep -o '([0-9]+\.){2}[0-9]+'))
 else 
-    echo $(git branch -a | egrep -o '([0-9]+\.){2}[0-9]+')
+    semanticVersion=$(echo $(git branch -a | egrep -o '([0-9]+\.){2}[0-9]+'))
 fi
+
+echo 'semanticVersion var: '$semanticVersion
+
+
+echo '{"hostname":"test","domainname":"example.com"}' 
