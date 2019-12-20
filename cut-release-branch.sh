ee8ea398ebdfd0f0b9265a7c6b11bb745796c300
Jessica Wilson <jwilson@zipwhip.com>
2019-12-20 11:44:30 -0800

    debugging

--- cut-release-branch.sh
+++ cut-release-branch.sh
@@ -4,11 +4,20 @@ set -e
 regexString=$1
 
 semanticVersion=''
-if [ $regexString ]; then 
-    semanticVersion=$(echo $(git branch -a | grep $regexString | egrep -o '([0-9]+\.){2}[0-9]+'))
-else 
-    semanticVersion=$(echo $(git branch -a | egrep -o '([0-9]+\.){2}[0-9]+'))
-fi
+function getLastSemanticVersion {
+    if [ $regexString ]; then 
+        semanticVersions=$(echo $(git branch -a | grep $regexString | egrep -o '([0-9]+\.){2}[0-9]+'))
+    else 
+        semanticVersions=$(echo $(git branch -a | egrep -o '([0-9]+\.){2}[0-9]+'))
+    fi
+
+}
+getLastSemanticVersion
+
+    for var in $semanticVersion
+    do 
+        echo 'var: ' $var
+    done
 
 echo 'semanticVersion var: '$semanticVersion
 
