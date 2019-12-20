0a2541d418bc701e0a8e07adc7f719a774243823
Jessica Wilson <jwilson@zipwhip.com>
2019-12-20 10:54:32 -0800

    clean up

--- cut-release-branch.sh
+++ cut-release-branch.sh
@@ -3,6 +3,8 @@ set -e
 
 regexString=$1
 
+echo 'git for each: '$(git for-each-ref --sort=committerdate refs/heads/)
+
 if [ $regexString ]; then 
     echo $(git branch -a | grep $regexString | egrep -o '([0-9]+\.){2}[0-9]+')
 else 
