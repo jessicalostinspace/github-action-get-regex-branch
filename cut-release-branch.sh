dd5d5d513acd67a3a4cb828cd9ff963b6525d7fd
Jessica Wilson <jwilson@zipwhip.com>
2019-12-20 12:41:34 -0800

    Grabbing last sort elem

--- cut-release-branch.sh
+++ cut-release-branch.sh
@@ -15,6 +15,7 @@ fi
 # echo $(git branch -a | grep $regexString | egrep -o '([0-9]+\.){2}[0-9]+' | cut -d " ")
 
 echo 'semanticVersions var: '$semanticVersions
+echo 'branches: ' $(git branch -a)
 
 
 # echo '{"hostname":"test","domainname":"example.com"}' 
