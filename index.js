c304ae545d61f8c381dd74601cf65f3bff03d1a8
Jessica Wilson <jwilson@zipwhip.com>
2020-01-03 14:05:25 -0800

    Sanitizing user input

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const core = require('@actions/core');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const re = require('re');
 
 try {
   // `regex-string` input defined in action metadata file
   const regexString = core.getInput('regex-string');
+  if (re.match("^[A-Za-z0-9_-]*$", regexString)) {
+    const command = 'bash cut-release-branch.sh ' + regexString;
+  
+    const output = getSemVerBranches(command);
+    output.then(function(result){
+      if (result["semanticVersion"]) {
+        console.log('\x1b[32m%s\x1b[0m', `Last Semantic Version Found: ${result["semanticVersion"]}`);
+        core.setOutput("last-semver", result["semanticVersion"]);
+      }
+      if (result["branchName"]) {
+        console.log('\x1b[32m%s\x1b[0m', `Last Branch with Semantic Version Found: ${result["branchName"]}`);
+        core.setOutput("last-semver-branch", result["branchName"]);
+      }
+    });
+  } else {
+    const regexError = "Regex string must contain only numbers, strings, underscores, and dashes.";
+    console.log('\x1b[33m%s\x1b[0m', regexError);
+    core.setFailed(regexError);
+  }
 
-  const command = 'bash cut-release-branch.sh ' + regexString;
-
-  const output = getSemVerBranches(command);
-  output.then(function(result){
-    if (result["semanticVersion"]) {
-      console.log('\x1b[32m%s\x1b[0m', `Last Semantic Version Found: ${result["semanticVersion"]}`);
-      core.setOutput("last-semver", result["semanticVersion"]);
-    }
-    if (result["branchName"]) {
-      console.log('\x1b[32m%s\x1b[0m', `Last Branch with Semantic Version Found: ${result["branchName"]}`);
-      core.setOutput("last-semver-branch", result["branchName"]);
-    }
-  });
 } catch (error) {
   core.setFailed(error.message);
 }
