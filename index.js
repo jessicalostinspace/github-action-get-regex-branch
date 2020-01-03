27cab5f9f2ce556b367319c01f386e167238fec9
Jessica <jessicalostinspace@gmail.com>
2020-01-03 14:25:21 -0800

    Development (#4)
    * Added a test workflow
    
    * Added name for run step
    
    * Removed runtime instance
    
    * Running on push to test
    
    * Removed development flag for checkout option
    
    * Switched path to action
    
    * Moving action to github folder
    
    * Switch use location
    
    * Altering pathname to find action
    
    * another path alter
    
    * changing filepath without action/checkout move
    
    * Removed checkout action
    
    * Changed action path
    
    * Added js exec to execute git command
    
    * Sorted git branches by date commit
    
    * logged errors
    
    * Turned exec into variable
    
    * Set release default name
    
    * Console logged everything
    
    * typo
    
    * testing branch filter
    
    * Removed unused param
    
    * Moved git command as param
    
    * backticks
    
    * double qoutes around command
    
    * Added shell script to run git commands
    
    * Changed exec command
    
    * removing script header
    
    * Addind bash shell
    
    * Added execution permission on script
    
    * added script param, added regex
    
    * Force empty string
    
    * force parens
    
    * Adding ascii
    
    * clean up
    
    * Added sort to pipe
    
    * Grabbing last sort elem
    
    * added branch name to output
    
    * Added branch filter
    
    * Return version and branch
    
    * Returned stdout
    
    * Making output logs more clear
    
    * Cleanup
    
    * Removing logs
    
    * Sanitizing user input
    
    * Adding example workflow

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 try {
   // `regex-string` input defined in action metadata file
   const regexString = core.getInput('regex-string');
-  console.log(`Regex String: ${regexString}!`);
-
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const regexp = /^[A-Za-z0-9_-]*$/;
+  if (regexp.test(regexString)) {
+    const command = 'bash cut-release-branch.sh ' + regexString;
   
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const output = getSemVerBranches(command);
+    output.then(function(result){
+      if (result["semanticVersion"]) {
+        console.log('\x1b[32m%s\x1b[0m', `Last Semantic Version Found: ${result["semanticVersion"]}`);
+        core.setOutput("last-semver", result["semanticVersion"]);
+      }
+      if (result["branchName"]) {
+        console.log('\x1b[32m%s\x1b[0m', `Last Branch with Semantic Version Found: ${result["branchName"]}`);
+        core.setOutput("last-semver-branch", result["branchName"]);
+      }
+    });
+  } else {
+    const regexError = "Regex string must contain only numbers, strings, underscores, and dashes.";
+    console.log('\x1b[33m%s\x1b[0m', regexError);
+    core.setFailed(regexError);
+  }
+
 } catch (error) {
   core.setFailed(error.message);
+}
+
+async function getSemVerBranches(command) {
+  // Get all the branches with the regex prefix and return the last version
+  const { err, stdout, stderr } = await exec(command, [{ shell: "bash" }]);
+
+  if (err) {
+    console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
+    console.log('\x1b[31m%s\x1b[0m', stderr);
+    process.exit(1);
+
+    return;
+  }
+
+  const data = JSON.parse(stdout);
+  if (data) {
+    return data;
+  }
+
+  process.exit(0);
 }
