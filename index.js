78d73e6940d824f4df56c87e5fa98f3c85582613
Jessica Wilson <jwilson@zipwhip.com>
2020-01-03 13:04:43 -0800

    debugging

--- index.js
+++ index.js
@@ -8,36 +8,37 @@ try {
   console.log(`Regex String: ${regexString}`);
 
   const command = 'bash cut-release-branch.sh ' + regexString;
-  console.log('command:', command)
-  
+
   // Get all the branches with the regex prefix and return the last version
-    exec(command, [{shell: "bash"}], (err, stdout, stderr) => {
-        if (err) {
-            console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
-            console.log('\x1b[31m%s\x1b[0m', stderr);
-            process.exit(1);
+  const output = exec(command, [{ shell: "bash" }], (err, stdout, stderr) => {
+    if (err) {
+      console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
+      console.log('\x1b[31m%s\x1b[0m', stderr);
+      process.exit(1);
 
-            return;
-        }
-        
-        console.log('\x1b[32m%s\x1b[0m', `Found branch: ${stdout}`);
-        const data = JSON.parse(stdout); 
-        if (data) {
-            return data;
-        }
+      return;
+    }
 
-        // core.setOutput("release-branch-name", release-branch-name);
-        process.exit(0);
-        });
+    console.log('\x1b[32m%s\x1b[0m', `Found branch: ${stdout}`);
+    const data = JSON.parse(stdout);
+    if (data) {
+      return data;
+    }
 
-    // console.log('lastReleaseBranchName: ', lastReleaseBranchName)
-  
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
+    // core.setOutput("release-branch-name", release-branch-name);
+    process.exit(0);
+  });
+
+  console.log("output: ", output)
+
+  // console.log('lastReleaseBranchName: ', lastReleaseBranchName)
+
+  //   const time = (new Date()).toTimeString();
+  //   core.setOutput("time", time);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
+  //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
