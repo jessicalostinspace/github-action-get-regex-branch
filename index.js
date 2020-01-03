23585c47a82b0ac246a1ce0ddf787bc3df1daa47
Jessica Wilson <jwilson@zipwhip.com>
2020-01-03 13:12:57 -0800

    debugging

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
+
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 try {
   // `regex-string` input defined in action metadata file
@@ -9,8 +13,44 @@ try {
 
   const command = 'bash cut-release-branch.sh ' + regexString;
 
+  // // Get all the branches with the regex prefix and return the last version
+  // exec(command, [{ shell: "bash" }], (err, stdout, stderr) => {
+  //   if (err) {
+  //     console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
+  //     console.log('\x1b[31m%s\x1b[0m', stderr);
+  //     process.exit(1);
+
+  //     return;
+  //   }
+
+  //   console.log('\x1b[32m%s\x1b[0m', `Found branch: ${stdout}`);
+  //   const data = JSON.parse(stdout);
+  //   if (data) {
+  //     return data;
+  //   }
+
+  //   // core.setOutput("release-branch-name", release-branch-name);
+  //   process.exit(0);
+  // });
+
+  const output = getSemVerBranches(command);
+  console.log("output :", output)
+
+  // console.log('lastReleaseBranchName: ', lastReleaseBranchName)
+
+  //   const time = (new Date()).toTimeString();
+  //   core.setOutput("time", time);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //   console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+async function getSemVerBranches(command) {
   // Get all the branches with the regex prefix and return the last version
-  const output = exec(command, [{ shell: "bash" }], (err, stdout, stderr) => {
+  await exec(command, [{ shell: "bash" }], (err, stdout, stderr) => {
     if (err) {
       console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
       console.log('\x1b[31m%s\x1b[0m', stderr);
@@ -28,17 +68,4 @@ try {
     // core.setOutput("release-branch-name", release-branch-name);
     process.exit(0);
   });
-
-  console.log("output: ", output)
-
-  // console.log('lastReleaseBranchName: ', lastReleaseBranchName)
-
-  //   const time = (new Date()).toTimeString();
-  //   core.setOutput("time", time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //   console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
 }
