b7dfaffae3b82856c5a1185bf18c749515d94d4f
Jessica Wilson <jwilson@zipwhip.com>
2020-01-09 11:38:57 -0800

    Adding logging for script

--- get-semver-and-branch.sh
+++ get-semver-and-branch.sh
@@ -5,12 +5,15 @@ regexString=$1
 
 semanticVersion=''
 branchName=''
-if [ $regexString ]; then 
-    branchName=$(echo $(git branch -a | grep $regexString | grep -E '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1 | sed 's@.*/@@'))
-    semanticVersion=$(echo $(git branch -a | grep $regexString | egrep -o '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1))
+echo "regexString: " $regexString
+echo $(git branch -a)
+
+if [ "$regexString" ]; then 
+    branchName=$(git branch -a | grep "$regexString" | grep -E '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1 | sed 's@.*/@@')
+    semanticVersion=$(git branch -a | grep "$regexString" | egrep -o '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1)
 else 
-    branchName=$(echo $(git branch -a | grep -E '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1 | sed 's@.*/@@'))
-    semanticVersion=$(echo $(git branch -a | egrep -o '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1))
+    branchName=$(git branch -a | grep -E '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1 | sed 's@.*/@@')
+    semanticVersion=$(git branch -a | egrep -o '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1)
 fi
 
 echo '{"semanticVersion": "'$semanticVersion'", "branchName": "'$branchName'"}' 
