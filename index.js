7a4177c8fd3bbc1227de8733cda184ea4ea8d422
Jessica Wilson <jwilson@zipwhip.com>
2020-01-09 10:53:53 -0800

    Moved error logic

--- index.js
+++ index.js
@@ -8,26 +8,14 @@ try {
   // `regex-string` input defined in action metadata file
   const regexString = core.getInput('regex-string');
   const regexp = /^[A-Za-z0-9_-]*$/;
+
   if (regexp.test(regexString)) {
-  
-    const output = getSemVerBranches(regexString);
-    output.then(function(result){
-      console.log("result :", result)
-      if (result["semanticVersion"]) {
-        console.log('\x1b[32m%s\x1b[0m', `Last Semantic Version Found: ${result["semanticVersion"]}`);
-        core.setOutput("last-semver", result["semanticVersion"]);
-      }
-      if (result["branchName"]) {
-        console.log('\x1b[32m%s\x1b[0m', `Last Branch with Semantic Version Found: ${result["branchName"]}`);
-        core.setOutput("last-semver-branch", result["branchName"]);
-      }
-    });
+    getSemVerBranches(regexString);
   } else {
     const regexError = "Regex string must contain only numbers, strings, underscores, and dashes.";
     console.log('\x1b[33m%s\x1b[0m', regexError);
     core.setFailed(regexError);
   }
-
 } catch (error) {
   core.setFailed(error.message);
 }
@@ -42,24 +30,29 @@ async function getSemVerBranches(regexString) {
     options.listeners = {
       stdout: (data) => {
         output += data.toString();
-        console.log("output1: ", output)
       },
       stderr: (data) => {
         err += data.toString();
-        console.log("err1: ", err)
       }
     };
 
     await exec.exec(`${src}/get-semver-and-branch.sh`, [regexString], options);
-    console.log("output: ", output)
-    console.log("err: ", err)
+
+    if (output["semanticVersion"]) {
+      console.log('\x1b[32m%s\x1b[0m', `Last Semantic Version Found: ${output["semanticVersion"]}`);
+      core.setOutput("last-semver", output["semanticVersion"]);
+    }
+    if (output["branchName"]) {
+      console.log('\x1b[32m%s\x1b[0m', `Last Branch with Semantic Version Found: ${output["branchName"]}`);
+      core.setOutput("last-semver-branch", output["branchName"]);
+    }
+    if (err) {
+      console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
+      console.log('\x1b[31m%s\x1b[0m', err);
+      process.exit(1);
+    }
   } catch (err) {
     core.setFailed(err.message);
-      if (err) {
-        console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
-        console.log('\x1b[31m%s\x1b[0m', err);
-        process.exit(1);
-      }
     process.exit(0);
   }
 }
